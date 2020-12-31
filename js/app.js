a078464a6f8341cd5e682455929a1deee347f958
Chris M <habibdev96@gmail.com>
2020-12-30 20:37:16 -0700

    created hamburger menu

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// HAMBURGER MENU
+const menuIcon = document.querySelector(".hamburger-menu");
+const menuNavbar = document.querySelector(".menu-navbar");
+
+menuIcon.addEventListener("click", () => menuNavbar.classList.toggle("change"));
