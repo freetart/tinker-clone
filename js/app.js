c6c75f071aa0bfe31ba6ab5ab3cf3d9c028047f4
Chris M <habibdev96@gmail.com>
2021-01-02 14:38:35 -0700

    created modal & made responsive

--- js/app.js
+++ js/app.js
@@ -1,5 +1,35 @@
-// HAMBURGER MENU
+// hamburger menu
 const menuIcon = document.querySelector(".hamburger-menu");
 const menuNavbar = document.querySelector(".menu-navbar");
 
 menuIcon.addEventListener("click", () => menuNavbar.classList.toggle("change"));
+
+// modal
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
